556b5b9e47b169f2be0dadf797d71faa14737429
Yohan Apriyandi <cepoangkuningan@gmail.com>
2025-05-31 10:26:28 +0700

    belajar unit test d golang

---
+++ README.md
@@ -0,0 +1,4 @@
+Golang Unit Test
+
+Mock:
+Gunakan mock apabila kita akan melakukan test tehadap code yang sulit untuk dilakukan test, seperti integrasi layanan third party, get data from database dan sejenisnya dimana kita tidak memungkinkan untuk mengakses resource secara langsung
---
+++ helper/hello_word_test.go
@@ -0,0 +1,134 @@
+package helper
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"runtime"
+	"testing"
+)
+
+func TestTableHelloWord(t *testing.T) {
+	tests := []struct {
+		name     string
+		request  string
+		expected string
+	}{
+		{
+			name:     "Yohan",
+			request:  "Yohan",
+			expected: "Hello Yohan",
+		},
+		{
+			name:     "Nabila",
+			request:  "Nabila",
+			expected: "Hello Nabila",
+		},
+		{
+			name:     "Fathar",
+			request:  "Fathar",
+			expected: "Hello Fathar",
+		},
+		{
+			name:     "Taqy",
+			request:  "Taqy",
+			expected: "Hello Taqy",
+		},
+		{
+			name:     "Fithriya",
+			request:  "Fithriya",
+			expected: "Hello Fithriya",
+		},
+		{
+			name:     "Apriyandi",
+			request:  "Apriyandi",
+			expected: "Hello Apriyandi",
+		},
+		{
+			name:     "Fariq",
+			request:  "Fariq",
+			expected: "Hello Fariq",
+		},
+		{
+			name:     "Dhabit",
+			request:  "Dhabit",
+			expected: "Hello Dhabit",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := HelloWord(test.request)
+			require.Equal(t, test.expected, result, "Berhasil di eksekusi")
+		})
+	}
+	fmt.Println("Berhasil diksekusi testnya")
+}
+
+func TestSubTest(t *testing.T) {
+	t.Run("Yohan", func(t *testing.T) {
+		result := HelloWord("Yohan")
+		require.Equal(t, "Hello Yohan", result, "Result must be 'Hello Yohan'")
+	})
+	t.Run("Apriyandi", func(t *testing.T) {
+		result := HelloWord("Apriyandi")
+		require.Equal(t, "Hello Apriyandi", result, "Result must be 'Hello Apriyandi'")
+	})
+}
+
+func TestMain(m *testing.M) {
+	// after
+	fmt.Println("Before unit test")
+
+	m.Run()
+
+	// before
+	fmt.Println("After unit test")
+
+}
+
+func TestSkip(t *testing.T) {
+	if runtime.GOOS == "linux" {
+		t.Skip("Can't run on mac OS")
+	}
+	result := HelloWord("Yohan")
+	assert.Equal(t, "Hello Yohan", result, "Result must be 'Hello Yohan'")
+	fmt.Println("Test with t.Skip done")
+}
+
+func TestHelloWordAssert(t *testing.T) {
+	//go get github.com/stretchr/testify/assert@v1.10.0 intall it if u use assert
+	result := HelloWord("Yohan")
+	assert.Equal(t, "Hello Yohan", result, "Result must be 'Hello Yohan'")
+	fmt.Println("Test with assertion done")
+}
+
+func TestHelloWordRequire(t *testing.T) {
+	//go get github.com/stretchr/testify/assert@v1.10.0 intall it if u use assert
+	result := HelloWord("Yohan")
+	require.Equal(t, "Hello Yohan", result, "Result must be 'Hello Yohan'")
+	fmt.Println("Test with require done")
+}
+
+func TestHelloWord(t *testing.T) {
+	result := HelloWord("Yohan")
+	if result != "Hello Yohan" {
+		//panic("result it not Hello Yohan")
+		//t.Fail()
+		//t.Error() bisa diberikan argumen sehingga ketika ada error lebih mudah dibaca
+		//t.Error akan memanggil t.fail()
+		t.Error("result must be Hello Yohan")
+	}
+	fmt.Println("Test hello word done")
+}
+
+func TestHelloFathar(t *testing.T) {
+	result := HelloWord("Fathar")
+	if result != "Hello Fathar" {
+		//t.FailNow()
+		//t.Fatal() bisa diberikan argumen sehingga ketika ada error lebih mudah dibaca
+		//t.Error akan memanggil t.FailNow()
+		t.Fatal("result must be Hello fathar")
+	}
+	fmt.Println("Test hello word fathar done")
+}
